be221db793841ab555a60265844b54a04d6a9c86
kfjt <kfjt@users.noreply.github.com>
2025-02-06 12:58:48 +0900

    Add functionality to dynamically add subtasks in JsonFormsDemo component

--- src/components/JsonFormsDemo.tsx
+++ src/components/JsonFormsDemo.tsx
@@ -4,14 +4,22 @@ import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {
+  GroupLayout,
+  isControl,
+  isGroup,
+  JsonSchema,
+  Layout,
+  UISchemaElement,
+} from '@jsonforms/core';
+import {
   materialCells,
   materialRenderers,
 } from '@jsonforms/material-renderers';
 import RatingControl from './RatingControl';
 import ratingControlTester from '../ratingControlTester';
 import AccordionLayout, { AccordionLayoutTester } from './AccordionLayout';
-import schema from '../schema.json';
-import uischema from '../uischema.json';
+import initialSchema from '../schema.json';
+import initialUischema from '../uischema.json';
 
 const classes = {
   container: {
@@ -58,13 +66,90 @@ const renderers = [
   { tester: AccordionLayoutTester, renderer: AccordionLayout },
 ];
 
+const findAndInsertSubstack = (
+  uischemaElements: UISchemaElement[],
+  count: number,
+) => {
+  let updatedElements = [...uischemaElements];
+  for (let i = 0; i < updatedElements.length; i++) {
+    const element = updatedElements[i];
+    if (
+      isGroup(element as Layout) &&
+      (element as GroupLayout).label === 'Subtask'
+    ) {
+      updatedElements.splice(i + 1, 0, {
+        ...element,
+        label: `Subtask ${count}`,
+        elements: [
+          ...(element as Layout).elements.map(el => {
+            if (isControl(el)) {
+              return {
+                ...el,
+                scope: el.scope.replace(/subtask/g, `subtask__ADD_${count}`),
+              };
+            } else {
+              return el;
+            }
+          }),
+        ],
+      } as UISchemaElement);
+      return updatedElements;
+    }
+
+    const layoutElement = element as Layout;
+    if (layoutElement.elements) {
+      const nestedElements = findAndInsertSubstack(
+        layoutElement.elements,
+        count,
+      );
+      if (nestedElements !== layoutElement.elements) {
+        updatedElements[i] = {
+          ...layoutElement,
+          elements: nestedElements,
+        } as Layout;
+      }
+    }
+  }
+  return updatedElements;
+};
+
 export const JsonFormsDemo: FC = () => {
+  const [schema, setSchema] = useState<JsonSchema>(initialSchema);
+  const [uischema, setUischema] = useState<UISchemaElement>(initialUischema);
   const [data, setData] = useState<object>(initialData);
   const stringifiedData = useMemo(() => JSON.stringify(data, null, 2), [data]);
 
   const clearData = () => {
     setData({});
   };
+
+  const addSubTask = () => {
+    console.log('add subtask');
+    const updatedSchema = { ...schema };
+    const subtaskKeyBase = 'subtask';
+    let count = 1;
+
+    if (!updatedSchema.properties) {
+      updatedSchema.properties = {};
+    }
+    while (updatedSchema.properties[`${subtaskKeyBase}__ADD_${count}`]) {
+      count++;
+    }
+
+    updatedSchema.properties[`${subtaskKeyBase}__ADD_${count}`] = {
+      ...updatedSchema.properties[subtaskKeyBase],
+      title: `Subtask ${count}`,
+    };
+    setSchema(updatedSchema);
+
+    const updatedUischema = { ...uischema } as Layout;
+    updatedUischema.elements = findAndInsertSubstack(
+      updatedUischema.elements,
+      count,
+    );
+    setUischema(updatedUischema);
+  };
+
   return (
     <Grid
       container
@@ -87,6 +172,9 @@ export const JsonFormsDemo: FC = () => {
       </Grid>
       <Grid item sm={6}>
         <Typography variant={'h4'}>Rendered form</Typography>
+        <Button onClick={addSubTask} color="secondary" variant="contained">
+          Add subtask
+        </Button>
         <div style={classes.demoform}>
           <JsonForms
             schema={schema}
